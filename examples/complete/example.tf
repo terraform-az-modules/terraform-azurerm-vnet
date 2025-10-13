ea353ea4ca40ff81dfffe64d44097b5666db5f97
Ayush Sharma <ayush2001khandal@gmail.com>
2025-10-13 17:26:14 +0530

    fix: updated module versions

--- examples/complete/example.tf
+++ examples/complete/example.tf
@@ -8,7 +8,7 @@ provider "azurerm" {
 ##-----------------------------------------------------------------------------
 module "resource_group" {
   source      = "terraform-az-modules/resource-group/azure"
-  version     = "1.0.0"
+  version     = "1.0.1"
   name        = "core"
   environment = "dev"
   location    = "centralus"
