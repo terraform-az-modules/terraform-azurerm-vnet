795b9b5b3c37c8791cd3f33aa6fb02b4e110d81d
Ravi Malvia <ravi.malvia@clouddrove.com>
2025-11-05 17:20:06 +0530

    fix: Updated tags and source of the modules

--- examples/complete/example.tf
+++ examples/complete/example.tf
@@ -7,8 +7,8 @@ provider "azurerm" {
 ## Resource group in which all resources will be deployed.
 ##-----------------------------------------------------------------------------
 module "resource_group" {
-  source      = "terraform-az-modules/resource-group/azure"
-  version     = "1.0.1"
+  source      = "terraform-az-modules/resource-group/azurerm"
+  version     = "1.0.3"
   name        = "core"
   environment = "dev"
   location    = "centralus"
