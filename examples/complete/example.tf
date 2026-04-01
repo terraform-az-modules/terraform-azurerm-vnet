946227e9a2a5c93384b402baa9cf73d0c835d584
karantolambiya-cd <karan.tolambiya28.com>
2026-04-01 18:26:59 +0530

    fix: removed the commented code

--- examples/complete/example.tf
+++ examples/complete/example.tf
@@ -20,13 +20,6 @@ module "resource_group" {
 ##-----------------------------------------------------------------------------
 module "vnet" {
   source = "../../"
-  # name   = "core"
-  # # custom_name            = ""  # Optional: Overrides default naming logic with a fully custom name. Cannot be used if `name` is set.
-  # environment         = "dev"
-  # label_order         = ["name", "environment", "location"] # locations logic are pre configured in the labels module
-  # resource_group_name = module.resource_group.resource_group_name
-  # location            = module.resource_group.resource_group_location
-  # address_spaces      = ["10.0.0.0/16"]
   enable      = true
   name        = "core"
   environment = "dev"
